570d1eb604e8769fb3153414fe763036a202fc6c
daeri-milk <rand_abelar@gmail.com>
2025-03-17 00:04:09 +0800

    Created NavBar, themeData, Colors. Do not touch main.dart, currently under debugging.

---
+++ frontend/pms_frontend/lib/theme/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+class ThemeColor {
+  ThemeColor._();
+  static const Color primaryColor = Color(0xFF181512);
+  static const Color secondaryColor = Color(0xFFA67B3D);
+  static const Color grey = Color(0xFF8F8F8F);
+  static const Color red = Color(0xFFA81818);
+  static const Color white = Color(0xFFF4F4F4);
+  static const Color white2 = Color(0xFFF7F7F7);
+}
---
+++ frontend/pms_frontend/lib/widget/navbar.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import '../theme/themedata.dart';
+import '../theme/colors.dart';
+
+class Navbar extends StatelessWidget {
+  const Navbar({super.key});
+
+  @override
+  Widget build (BuildContext context) {
+    return Scaffold(
+      backgroundColor: ThemeColor.white,
+      body: Padding(
+          padding: const EdgeInsets.all(3.0),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Container(
+              constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    width: MediaQuery.of(context).size.width,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      color: ThemeColor.white2,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.1),
+                          spreadRadius: 100,
+                          blurRadius: 0,
+                          offset: const Offset(0, 0)
+                        ),
+                      ]
+                    ),
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Text('placeholder text', style: TextStyle(fontSize: 20, color: ThemeColor.primaryColor, fontWeight: FontWeight.bold,)),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+      ),
+    );
+  }
+}
